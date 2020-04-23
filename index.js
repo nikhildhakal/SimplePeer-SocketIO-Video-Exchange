cd2fa52b0c39731f3c056b2b928b8ed164b16155
unknown <nickarsenal007@gmail.com>
2020-04-23 13:08:36 +0545

    Start working app

---
+++ index.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	socket.on('datastring', (id) => {
+		io.emit('datastring', id);
+	});
+});
+
+http.listen(3000, () => {
+	console.log('listening on *:3000');
+});
